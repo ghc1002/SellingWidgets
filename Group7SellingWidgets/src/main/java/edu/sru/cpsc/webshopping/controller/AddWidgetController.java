906e751ebd43d45cbb4d288d27476a408bec4e77
kas1064 <kas1064 kas1064@sru.edu>
2023-02-13 13:00:38 -0500

    close #31
    create a listing now checks to ensure price is greater than zero

--- Group7SellingWidgets/src/main/java/edu/sru/cpsc/webshopping/controller/AddWidgetController.java
+++ Group7SellingWidgets/src/main/java/edu/sru/cpsc/webshopping/controller/AddWidgetController.java
@@ -51,6 +51,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigDecimal;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -1239,6 +1240,13 @@ public String addCar(Model model)
 			result.addError(
 	    	          new FieldError("pricePerItem", "pricePerItem", "Price per item can't be null"));	    	
 	       }
+		// check if the price of the item is greater than zero (should only fire after price is verified not null)
+		else if(marketListing.getPricePerItem().compareTo(BigDecimal.ZERO) != 1) {
+			setPage("error2");
+			result.addError(
+					new FieldError("pricePerItem", "pricePerItem", "Price per item must be greater than zero"));
+		}
+		
 		if(Long.valueOf(marketListing.getQtyAvailable()).compareTo((long) 0) <= 0)  {
 			System.out.println(marketListing.getQtyAvailable());
 			System.out.println("help");
