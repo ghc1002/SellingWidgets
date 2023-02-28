14d0feb8bcbe7833149356b4b2232d2278127066
kas1064 <kas1064 kas1064@sru.edu>
2023-02-28 09:14:49 -0500

    unfinished work on tabs
    just pushing because it's tuesday, this is unfinished

---
+++ Group7SellingWidgets/src/main/java/edu/sru/cpsc/webshopping/controller/sidebar/SidebarController.java
@@ -0,0 +1,7 @@
+package edu.sru.cpsc.webshopping.controller.sidebar;
+
+import edu.sru.cpsc.webshopping.repository.sidebar.SidebarRepository;
+
+public class SidebarController {
+	private SidebarRepository sidebarRepository;
+}
---
+++ Group7SellingWidgets/src/main/java/edu/sru/cpsc/webshopping/repository/sidebar/SidebarRepository.java
@@ -0,0 +1,9 @@
+package edu.sru.cpsc.webshopping.repository.sidebar;
+
+import edu.sru.cpsc.webshopping.domain.sidebar.Sidebar;
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SidebarRepository extends CrudRepository<Sidebar, Long> {
+
+}
